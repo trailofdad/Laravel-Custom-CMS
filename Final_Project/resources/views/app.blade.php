c10a723251757159997af9abf7f4f6c736aaddcf
findlayhi <w0269118@nscc.ca>
2015-12-31 13:06:36 -0800

    selectivize now works.

--- Final_Project/resources/views/app.blade.php
+++ Final_Project/resources/views/app.blade.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css"/>
     <link rel="stylesheet" href="/public/css/bootstrap.min.css"/>
-        <link href="{{ url('vendor/selectize/css/selectize.bootstrap3.css') }}" rel="stylesheet">
-
+    {{--<link href="{{ url('vendor/selectize/css/selectize.bootstrap3.css') }}" rel="stylesheet">--}}
+    <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css" rel="stylesheet" />
 </head>
 
 <body>
@@ -22,21 +22,18 @@
     @yield('content')
 
 </div>
-
+<script language="JavaScript" type="text/javascript" src='{{ url("js/main.js") }}'></script>
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-<link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css" rel="stylesheet" />
-<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
 
-<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
 @yield('footer')
 <script type="text/javascript">
     var root = '{{url("/")}}';
 </script>
-<script language="JavaScript" type="text/javascript" src='//code.jquery.com/jquery-1.10.2.min.js'></script>
-<script language="JavaScript" type="text/javascript" src='{{ url("vendor/selectize/js/standalone/selectize.min.js") }}'></script>
-<script language="JavaScript" type="text/javascript" src='{{ url("js/main.js") }}'></script>
+{{--<script language="JavaScript" type="text/javascript" src='{{ url("vendor/selectize/js/standalone/selectize.min.js") }}'></script>--}}
+<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
+
 @yield('scripts')
 
 
